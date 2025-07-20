69a2ac28c105440f57b1914468807084c8ece66d
montenbruck <leonelalvarezmontenbruck@gmail.com>
2025-07-20 00:56:18 -0600

    update production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,8 @@
 // Este archivo es el entorno por defecto para desarrollo.
 // La URL de la API apuntará a tu backend local durante el desarrollo.
 export const environment = {
-  production: false, // Indica que no es el entorno de producción
-  testing: false,    // Indica que no es el entorno de testing
-  // URL de tu API de backend cuando desarrollas localmente.
-  // Asegúrate de que coincida con el puerto donde se ejecutará tu backend NestJS (por defecto 3000).
-  apiUrl: 'http://localhost:3000/api' // '/api' es el prefijo global que configuraremos en NestJS
+  production: true, // Indica que es el entorno de producción
+  testing: false,
+  // URL de tu API de backend en Render
+  apiUrl: 'https://login-app-backend-27xz.onrender.com/api'
 };
